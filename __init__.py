13037a307504cfc6a5758e61e6aa8e47d400e6cf
CHENARAMKHICHAR2003 <crchoudharyji1122@gmail.com>
2025-09-03 15:04:56 +0530

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,4 @@
+from .modules import appx_v1, classplus, visionias, testbook, pw
+
+def main():
+    print("Extractor module running...")
---
+++ main.py
@@ -0,0 +1,4 @@
+from Extractor import main
+
+if __name__ == "__main__":
+    main()

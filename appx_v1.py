f3aa481e2caea70266aa6330a19861a54e08f8bf
CHENARAMKHICHAR2003 <crchoudharyji1122@gmail.com>
2025-09-03 15:27:37 +0530

    Delete appx_v1.py

--- appx_v1.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-def download_appx(url, token):
-    headers = {"Authorization": f"Bearer {token}"}
-    resp = requests.get(url, headers=headers)
-    if resp.status_code == 200:
-        return resp.content
-    else:
-        raise Exception(f"Failed to fetch: {resp.status_code}")
